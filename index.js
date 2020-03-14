b9bf3762b5ecb38cecdd0beb632e11d82d102194
shourya <shouryaraj1999@gmail.com>
2020-03-14 14:44:37 +1100

    initial commit

---
+++ index.js
@@ -0,0 +1,18 @@
+const express = require('express');     // just like the import express from ''express'
+const app = express();
+
+// route handler
+// "/" watch requests trying to access
+// get => GET INFO
+// req => request (object representing the incoming request)
+// res => Object representing the outgoing response
+app.get('/', (req, res) =>{
+    res.send({hi: 'there'});
+});
+// Dynamic port binding
+const PORT = process.env.PORT || 5000;
+
+// enviroment operation by the service or else default set to 5000
+// app.listen(5000);
+app.listen(PORT);
+
